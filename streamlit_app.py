86697629bc77e35ca4d80cbd531eed205ea5be6b
gowtham-dm <gowthamm@decisionminds.com>
2026-04-28 15:13:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,58 +1,57 @@
-# Import python packages
+# Import python packages.
 import streamlit as st
+from snowflake.snowpark.functions import col
+import requests  
+import pandas as pd
 
-
-# Write directly to the app
-st.title(f" :cup_with_straw: Customize your smoothie :cup_with_straw: ")
+# Write directly to the app.
+st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
-  """choose the fruit you want in your custom smoothie
-  """
+  """Choose the fruits you want in your custom Smoothie!"""
 )
 
-name_on_order = st.text_input("Name on smoothie:")
-st.write("The name on the smoothie will be : ",name_on_order )
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your smoothie will be: ',name_on_order)
 
-from snowflake.snowpark.functions import col 
- 
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
-
-# ✅ Convert to Pandas (THIS LINE WAS MISSING)
-df = my_dataframe.to_pandas()
-
-# Multiselect widget
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
-    df["FRUIT_NAME"].tolist()
-    ,max_selections=5
-)
-if len(ingredients_list) > 5:
-    st.error("You can select up to 5 ingredients only!")
-
-if ingredients_list:
-
-    ingredients_string = ''
-    
-    for fruit_chosen in ingredients_list:
-       ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_list)
-
-    my_insert_stmt = """ 
-    INSERT INTO smoothies.public.orders (ingredients,name_on_order)
-    VALUES ('""" + ingredients_string + """','""" + name_on_order + """')
-"""
-    #st.write(my_insert_stmt)
-   # st.stop()
-
-    
-    time_to_insert=st.button('Sumbit Order')
-
-    if time_to_insert and name_on_order:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered,'+ name_on_order, icon="✅")
-
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
+#st.dataframe(data=my_dataframe, use_container_width = True)
+#st.stop()
+# covert the snowflake dataframe to python dataframe so we can use the loc function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+
+ingredient_list = st.multiselect(
+    'Choose up to 5 ingredients: ',my_dataframe,max_selections=5
+)
+
+if ingredient_list:
+    #st.write(ingredient_list)
+    #st.text(ingredient_list)
+    ingredients_string = ''
+    for each_fruit in ingredient_list:
+        ingredients_string += each_fruit + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', each_fruit,' is ', search_on, '.')
+      
+        st.subheader(each_fruit + ' Nutrition Information')
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+    #st.write(ingredients_string)
+    
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                    values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+
+    time_to_insert = st.button("Submit Order")
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
+
+
+
+ 
